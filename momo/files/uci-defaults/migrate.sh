ec72c66e82864d219cbefb3e9336a6020b016749
Joseph Mory <morytyann@outlook.com>
2025-12-29 11:33:45 +0800

    feat: add procd config
    1. move fast reload option to procd config
    2. support rlimit config
    
    Signed-off-by: Joseph Mory <morytyann@outlook.com>

--- momo/files/uci-defaults/migrate.sh
+++ momo/files/uci-defaults/migrate.sh
@@ -15,6 +15,12 @@ proxy_bypass_china_mainland_ip6=$(uci -q get momo.proxy.bypass_china_mainland_ip
 routing_tproxy_fw_mask=$(uci -q get momo.routing.tproxy_fw_mask); [ -z "$routing_tproxy_fw_mask" ] && uci set momo.routing.tproxy_fw_mask=0xFF
 routing_tun_fw_mask=$(uci -q get momo.routing.tun_fw_mask); [ -z "$routing_tun_fw_mask" ] && uci set momo.routing.tun_fw_mask=0xFF
 
+procd=$(uci -q get momo.procd); [ -z "$procd" ] && {
+	uci set momo.procd=procd
+	uci set momo.procd.fast_reload=$(uci -q get momo.config.fast_reload)
+	uci del momo.config.fast_reload
+}
+
 # commit
 uci commit momo
 
